2264e3da453a83450a053a2c9f983bdc6c539653
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-04 09:12:52 +0900

    31. 다운 캐스팅과 instanceof

--- chapter07/src/polymorphism/AnimalTest.java
+++ chapter07/src/polymorphism/AnimalTest.java
@@ -1,11 +1,16 @@
 package polymorphism;
 
+import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
 
 class Animal {
 
     public void move() {
         System.out.println("동물이 움직입니다.");
+    }
+
+    public void eating() {
+
     }
 }
 
@@ -27,6 +32,10 @@ class Tiger extends Animal {
     public void move() {
         System.out.println("호랑이가 네 발로 뜁니다.");
     }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을 합니다.");
+    }
 }
 
 class Eagle extends Animal {
@@ -34,6 +43,10 @@ class Eagle extends Animal {
     @Override
     public void move() {
         System.out.println("독수리가 하늘을 날아갑니다.");
+    }
+
+    public void flying() {
+        System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
     }
 }
 
@@ -43,6 +56,15 @@ public class AnimalTest {
         Animal hAmimal = new Human();
         Animal tAmimal = new Tiger();
         Animal eAmimal = new Eagle();
+
+//        Eagle human = (Eagle)hAmimal;
+        if (hAmimal instanceof Human) {
+            Human human = (Human)hAmimal;
+            human.readBooks();
+        }
+
+//        Human human = (Human)hAmimal;
+//        human.readBooks();
 
 //        AnimalTest test = new AnimalTest();
 //        test.moveAnimal(hAmimal);
@@ -54,8 +76,30 @@ public class AnimalTest {
         animalList.add(tAmimal);
         animalList.add(eAmimal);
 
-        for (Animal animal : animalList) {
-            animal.move();
+        AnimalTest test = new AnimalTest();
+        test.testDownCasting(animalList);
+
+//        for (Animal animal : animalList) {
+//            animal.move();
+//        }
+    }
+
+    public void testDownCasting(ArrayList<Animal> animalList) {
+        for (int i = 0; i < animalList.size(); i++) {
+            Animal animal = animalList.get(i);
+
+            if (animal instanceof Human) {
+                Human human = (Human)animal;
+                human.readBooks();
+            } else if (animal instanceof Tiger) {
+                Tiger tiger = (Tiger)animal;
+                tiger.hunting();
+            } else if (animal instanceof Eagle) {
+                Eagle eagle = (Eagle)animal;
+                eagle.flying();
+            } else {
+                System.out.println("error");
+            }
         }
     }
 
