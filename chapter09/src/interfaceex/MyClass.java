374898815384ddaba20a7dc6593d222c5f581c2c
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-10 07:24:11 +0900

    41. 인터페이스의 요소들 - 2

---
+++ chapter09/src/interfaceex/MyClass.java
@@ -0,0 +1,19 @@
+package interfaceex;
+
+public class MyClass implements MyInterface {
+
+    @Override
+    public void myMethod() {
+
+    }
+
+    @Override
+    public void x() {
+
+    }
+
+    @Override
+    public void y() {
+
+    }
+}
