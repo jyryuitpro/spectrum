e95028852b17d69a1941d1bc349e4cf84d50ecc1
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-11 07:50:46 +0900

    45. Object 클래스 - 3

--- chapter10/src/object/EqualsTest.java
+++ chapter10/src/object/EqualsTest.java
@@ -1,5 +1,7 @@
 package object;
 
+import java.util.Objects;
+
 class Student {
 
     int studentNum;
@@ -24,6 +26,11 @@ class Student {
         }
         return false;
     }
+
+    @Override
+    public int hashCode() {
+        return studentNum;
+    }
 }
 
 public class EqualsTest {
@@ -39,10 +46,20 @@ public class EqualsTest {
         Student Lee2 = Lee;
         Student Shin = new Student(100, "이순신");
 
-        System.out.println(Lee == Shin);
-        System.out.println(Lee.equals(Shin));
+//        System.out.println(Lee == Shin);
+//        System.out.println(Lee.equals(Shin));
 
-        System.out.println(Lee.hashCode());
-        System.out.println(Shin.hashCode());
+//        System.out.println(Lee.hashCode());
+//        System.out.println(Shin.hashCode());
+
+        Integer i1 = new Integer(100);
+        Integer i2 = 100;
+
+        System.out.println(i1.equals(i2));
+        System.out.println(i1.hashCode());
+        System.out.println(i2.hashCode());
+
+        System.out.println(System.identityHashCode(i1));
+        System.out.println(System.identityHashCode(i2));
     }
 }
--- chapter10/src/object/ToStringTest.java
+++ chapter10/src/object/ToStringTest.java
@@ -1,12 +1,46 @@
 package object;
 
+class Book implements Cloneable {
+
+    String title;
+
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+}
+
+
 public class ToStringTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Book book = new Book("토지", "박경리");
-        System.out.println(book);
+//        System.out.println(book);
 
         String str = new String("토지");
-        System.out.println(str.toString());
+//        System.out.println(str.toString());
+
+        Book book2 = (Book)book.clone();
+        System.out.println(book2);
     }
 }
