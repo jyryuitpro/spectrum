704171f60fbef6af9d6af08d0a071aa3242eac31
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-11 07:32:27 +0900

    44. Object 클래스 - 2

--- chapter10/src/object/ToStringTest.java
+++ chapter10/src/object/ToStringTest.java
@@ -1,25 +1,5 @@
 package object;
 
-class Book {
-
-    String title;
-
-    String author;
-
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
-}
-
 public class ToStringTest {
 
     public static void main(String[] args) {
