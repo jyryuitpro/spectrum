70ce7ccd2c77d7a664d7848cbcf8860609a21a4c
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-14 21:11:29 +0900

    51. List 인터페이스

---
+++ chapter11/src/collection/LinkedListTest.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        LinkedList<String> myList = new LinkedList<>();
+        myList.add("A");
+        myList.add("B");
+        myList.add("C");
+
+        System.out.println(myList);
+        myList.add(1, "D");
+        System.out.println(myList);
+        myList.removeLast();
+        System.out.println(myList);
+
+        for (int i = 0; i < myList.size(); i++) {
+            String s = myList.get(i);
+            System.out.println(s);
+        }
+    }
+}
