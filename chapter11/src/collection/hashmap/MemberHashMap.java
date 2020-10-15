2c9a8724a70f68860eeb643c109330c7b9a93513
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-16 07:41:28 +0900

    55. Map 인터페이스

---
+++ chapter11/src/collection/hashmap/MemberHashMap.java
@@ -0,0 +1,37 @@
+package collection.hashmap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class MemberHashMap {
+
+    private HashMap<Integer, Member> hashMap;
+
+    public MemberHashMap() {
+        hashMap = new HashMap<>();
+    }
+
+    public void addMember(Member member) {
+        hashMap.put(member.getMemberId(), member);
+    }
+
+    public boolean removeMember(int memberId) {
+        if (hashMap.containsKey(memberId)) {
+            hashMap.remove(memberId);
+            return true;
+        } else {
+            System.out.println("회원번호가 없습니다.");
+            return false;
+        }
+    }
+
+    public void showAllMember() {
+        Iterator<Integer> ir = hashMap.keySet().iterator();
+        while (ir.hasNext()) {
+            int key = ir.next();
+            Member member = hashMap.get(key);
+            System.out.println(member);
+        }
+        System.out.println();
+    }
+}
