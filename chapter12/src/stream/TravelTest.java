fb966bccc34112db96d48e36e7fd3706065b8aca
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-19 20:07:46 +0900

    62. 코딩해 보세요

---
+++ chapter12/src/stream/TravelTest.java
@@ -0,0 +1,29 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TravelTest {
+
+    public static void main(String[] args) {
+        TravelCustomer customerLee = new TravelCustomer("이순신", 40, 100);
+        TravelCustomer customerKim = new TravelCustomer("김유신", 20, 100);
+        TravelCustomer customerHong = new TravelCustomer("홍길동", 13, 50);
+
+        List<TravelCustomer> customerList = new ArrayList<>();
+        customerList.add(customerLee);
+        customerList.add(customerKim);
+        customerList.add(customerHong);
+
+        System.out.println(customerList);
+
+        customerList.stream().map(travelCustomer -> travelCustomer.getName()).forEach(s -> System.out.println(s));
+        int total = customerList.stream().mapToInt(c -> c.getPrice()).sum();
+        System.out.println(total);
+
+        customerList.stream()
+                .filter(travelCustomer -> travelCustomer.getAge() >= 20)
+                .map(travelCustomer -> travelCustomer.getName())
+                .sorted().forEach(s -> System.out.println(s));
+    }
+}
