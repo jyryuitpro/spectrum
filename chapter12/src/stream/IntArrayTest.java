372744127ef5f57f7d55978a50b45c99edf4cb97
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-19 19:40:35 +0900

    61. 스트림 - 2

--- chapter12/src/stream/IntArrayTest.java
+++ chapter12/src/stream/IntArrayTest.java
@@ -12,5 +12,7 @@ public class IntArrayTest {
 
         System.out.println(sum);
         System.out.println(count);
+
+        System.out.println(Arrays.stream(arr).reduce(0, (a, b) -> a + b));
     }
 }
