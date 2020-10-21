0accda95d336d70576ba5d046722cec08e928c0a
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-21 22:45:08 +0900

    67. 표준 입출력

---
+++ chapter14/src/stream/inputstream/SystemInTest.java
@@ -0,0 +1,18 @@
+package stream.inputstream;
+
+import java.io.IOException;
+
+public class SystemInTest {
+
+    public static void main(String[] args) {
+        System.out.println("입력:");
+
+        try {
+            int i = System.in.read();
+            System.out.println(i);
+            System.out.println((char)i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter14/src/stream/inputstream/SystemInTest2.java
@@ -0,0 +1,21 @@
+package stream.inputstream;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInTest2 {
+
+    public static void main(String[] args) {
+        System.out.println("입력 후 '끝' 이라고 쓰세요:");
+
+        try {
+            int i;
+            InputStreamReader isr = new InputStreamReader(System.in);
+            while ((i = isr.read()) != '끝') {
+                System.out.print((char)i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
