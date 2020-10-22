d5397853d4518bc494059f4c572547a04689e6a7
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-22 09:19:38 +0900

    74. Thread 구현하기

---
+++ chapter15/src/thread/ThreadTest.java
@@ -0,0 +1,60 @@
+package thread;
+
+//class MyThread extends Thread {
+//
+//    @Override
+//    public void run() {
+//        int i;
+//
+//        for (i=0; i<=200; i++) {
+//            System.out.print(i + "\t");
+//
+//            try {
+//                sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+
+class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        int i;
+
+        for (i=0; i<=200; i++) {
+            System.out.print(i + "\t");
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadTest {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+
+//        MyThread th1 = new MyThread();
+//        MyThread th2 = new MyThread();
+
+//        th1.start();
+//        th2.start();
+
+        MyThread runner1 = new MyThread();
+        Thread th1 = new Thread(runner1);
+        th1.start();
+
+        MyThread runner2 = new MyThread();
+        Thread th2 = new Thread(runner2);
+        th2.start();
+
+        System.out.println("end");
+    }
+}
