0d9063852b5a48adf2bead8fb807293957cef1fa
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-22 09:35:23 +0900

    75. Thread의 여러가지 메서드 활용 - 1

--- chapter15/src/thread/ThreadTest.java
+++ chapter15/src/thread/ThreadTest.java
@@ -47,13 +47,16 @@ public class ThreadTest {
 //        th1.start();
 //        th2.start();
 
-        MyThread runner1 = new MyThread();
-        Thread th1 = new Thread(runner1);
-        th1.start();
+//        MyThread runner1 = new MyThread();
+//        Thread th1 = new Thread(runner1);
+//        th1.start();
 
-        MyThread runner2 = new MyThread();
-        Thread th2 = new Thread(runner2);
-        th2.start();
+//        MyThread runner2 = new MyThread();
+//        Thread th2 = new Thread(runner2);
+//        th2.start();
+
+        Thread t = Thread.currentThread();
+        System.out.println(t);
 
         System.out.println("end");
     }
