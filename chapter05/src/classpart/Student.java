d5ba79e3cce9cc9a13ee5c25c23143d49eb9ad6b
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-23 07:11:31 +0900

    02. 클래스와 객체 - 02. 객체지향 프로그래밍과 클래스 -2

--- chapter05/src/classpart/Student.java
+++ chapter05/src/classpart/Student.java
@@ -4,5 +4,18 @@ class ABC {
 
 }
 
+//학생이 있습니다.
 public class Student {
+
+//    학생은 학번, 이름, 주소 값을 가집니다.
+    public int studentID;
+
+    public String studentName;
+
+    public String address;
+
+//    학생의 정보를 보여줍니다.
+    public void showStudentInfo() {
+        System.out.println(studentName + ", " + address);
+    }
 }
---
+++ chapter05/src/classpart/StudentTest.java
@@ -0,0 +1,11 @@
+package classpart;
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentName = "이순신";
+        studentLee.address = "서울";
+        studentLee.showStudentInfo();
+    }
+}
