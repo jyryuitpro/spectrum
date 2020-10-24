34f76e36cee86fbba41b630e84db0ea01bb8668f
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-25 08:07:32 +0900

    07. 클래스와 객체 - 07. 생성자, 생성자 오버로딩

--- chapter05/src/classpart/Student.java
+++ chapter05/src/classpart/Student.java
@@ -8,11 +8,29 @@ class ABC {
 public class Student {
 
 //    학생은 학번, 이름, 주소 값을 가집니다.
-    public int studentID;
+//    public int studentID;
+    private int studentID;
 
     public String studentName;
 
     public String address;
+
+//    오버로딩 : 이름이 같지만 매개변수가 다른 경우
+
+//    public Student() {}
+
+    public Student(String name) {
+        studentName = name;
+    }
+
+    public Student(int id, String name) {
+        studentID = id;
+        studentName = name;
+        address = "주소 없음";
+    }
+
+
+
 
 //    학생의 정보를 보여줍니다.
     public void showStudentInfo() {
--- chapter05/src/classpart/StudentTest.java
+++ chapter05/src/classpart/StudentTest.java
@@ -3,17 +3,20 @@ package classpart;
 public class StudentTest {
 
     public static void main(String[] args) {
-        Student studentLee = new Student();
-        studentLee.studentName = "이순신";
+//        Student studentLee = new Student();
+        Student studentLee = new Student("이순신");
+//        studentLee.studentName = "이순신";
         studentLee.address = "서울";
         studentLee.showStudentInfo();
 
-        Student studentKim = new Student();
-        studentKim.studentName = "김유신";
-        studentKim.address = "경주";
+//        Student studentKim = new Student();
+        Student studentKim = new Student(100, "김유신");
+//        studentKim.studentID = 100;
+//        studentKim.studentName = "김유신";
+//        studentKim.address = "경주";
         studentKim.showStudentInfo();
 
-        System.out.println(studentLee); // classpart.Student@3d494fbf
-        System.out.println(studentKim); // classpart.Student@1ddc4ec2
+//        System.out.println(studentLee); // classpart.Student@3d494fbf
+//        System.out.println(studentKim); // classpart.Student@1ddc4ec2
     }
 }
