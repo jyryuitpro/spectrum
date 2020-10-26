6fec4a74d57d2f101be3f1f5a3014a4bbac3b411
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-26 18:01:09 +0900

    09. 클래스와 객체 - 09. 정보 은닉 - 1

---
+++ chapter05/src/hiding/MyDate.java
@@ -0,0 +1,38 @@
+package hiding;
+
+public class MyDate {
+
+    private int day;
+
+    private int month;
+
+    private int year;
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void showDate() {
+        System.out.println(year + "년 " + month + "월 " + day + "일 입니다.private ");
+    }
+}
---
+++ chapter05/src/hiding/MyDateTest.java
@@ -0,0 +1,13 @@
+package hiding;
+
+public class MyDateTest {
+
+    public static void main(String[] args) {
+        MyDate myDate = new MyDate();
+//        myDate.day = 10;
+//        myDate.month = 7;
+//        myDate.year = 2019;
+
+        myDate.showDate();
+    }
+}
