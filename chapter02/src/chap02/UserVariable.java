3dc648181be0a03866d0376be43f57dac38c23ab
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-05 00:16:02 +0900

    Chapter 02-1. 변수의 이해와 활용

---
+++ chapter02/src/chap02/UserVariable.java
@@ -0,0 +1,13 @@
+package chap02;
+
+public class UserVariable {
+
+    public static void main(String[] args) {
+        int num1;
+        num1 = 10;
+
+        int num2 = 20;
+        int num3 = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + num3);
+    }
+}
