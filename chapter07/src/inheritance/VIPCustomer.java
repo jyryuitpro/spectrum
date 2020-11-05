55c64e065ff4495f2e28e7f79db75ed78401736a
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-05 09:22:43 +0900

    26. 상속이란 - 2

--- chapter07/src/inheritance/VIPCustomer.java
+++ chapter07/src/inheritance/VIPCustomer.java
@@ -7,24 +7,24 @@ public class VIPCustomer extends Customer {
     private int agentID;
 
 
-//    public VIPCustomer() {
-////        super();
-//        customerGrade = "VIP";
-//        bonusRatio = 0.05;
-//        salesRatio = 0.1;
-//
-//        System.out.println("VIPCustomer() 생성자 호출");
-//    }
-
-    public VIPCustomer(int customerID, String customerName) {
-        super(customerID, customerName);
-
+    public VIPCustomer() {
+//        super();
         customerGrade = "VIP";
         bonusRatio = 0.05;
         salesRatio = 0.1;
 
-//        System.out.println("VIPCustomer(int, String) 생성자 호출");
+        System.out.println("VIPCustomer() 생성자 호출");
     }
+
+//    public VIPCustomer(int customerID, String customerName) {
+//        super(customerID, customerName);
+//
+//        customerGrade = "VIP";
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//
+//        System.out.println("VIPCustomer(int, String) 생성자 호출");
+//    }
 
     @Override
     public int calcPrice(int price) {
