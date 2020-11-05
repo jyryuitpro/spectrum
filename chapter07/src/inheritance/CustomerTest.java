89fa2d358fd105ec55a3cd926330b55c7083ffa5
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-05 09:45:50 +0900

    27. 상속에서 클래스 생성 과정과 형 변환

--- chapter07/src/inheritance/CustomerTest.java
+++ chapter07/src/inheritance/CustomerTest.java
@@ -3,21 +3,24 @@ package inheritance;
 public class CustomerTest {
 
     public static void main(String[] args) {
-        Customer customerLee = new Customer();
-//        Customer customerLee = new Customer(10010, "이순신");
-        customerLee.setCustomerName("이순신");
-        customerLee.setCustomerID(10010);
+//        Customer customerLee = new Customer();
+        Customer customerLee = new Customer(10010, "이순신");
+//        customerLee.setCustomerName("이순신");
+//        customerLee.setCustomerID(10010);
         customerLee.bonusPoint = 1000;
         System.out.println(customerLee.showCustomerInfo());
 
-        VIPCustomer customerKim = new VIPCustomer();
+//        VIPCustomer customerKim = new VIPCustomer();
 //        VIPCustomer customerKim = new VIPCustomer(10020, "김유신");
-//        Customer customerKim = new VIPCustomer(10020, "김유신");
-        customerKim.setCustomerName("김유신");
-        customerKim.setCustomerID(10020);
+
+        //상위 클래스로의 묵시적 형 변환(업캐스팅)
+        Customer customerKim = new VIPCustomer(10020, "김유신");
+//        customerLee = customerKim;
+
+//        customerKim.setCustomerName("김유신");
+//        customerKim.setCustomerID(10020);
         customerKim.bonusPoint = 10000;
         System.out.println(customerKim.showCustomerInfo());
 
-//        customerLee = customerKim;
     }
 }
