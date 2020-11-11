64952b3e68beac67dcb07f2dabad641e95a42fb8
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-11 23:59:35 +0900

    33. 추상클래스란

--- chapter08/src/abstractex/Computer.java
+++ chapter08/src/abstractex/Computer.java
@@ -2,10 +2,12 @@ package abstractex;
 
 public abstract class Computer {
 
+    //추상 메서드
     public abstract void display();
 
     public abstract void typing();
 
+    //구현된 메서드 : 하위 클래스에 따라 재정의 할 수 있음 단, final로 메서드가 선언된 경우는 재정의 할 수 없음
     public void turnOn() {
         System.out.println("전원을 켭니다.");
     }
--- chapter08/src/abstractex/ComputerTest.java
+++ chapter08/src/abstractex/ComputerTest.java
@@ -3,7 +3,14 @@ package abstractex;
 public class ComputerTest {
 
     public static void main(String[] args) {
+
+        //인스턴스화 불가
+        //Computer computer = new Computer();
+
+        //묵시적 형변환
         Computer computer = new DeskTop();
+
+        //가상함수
         computer.display();
         computer.turnOff();
 
