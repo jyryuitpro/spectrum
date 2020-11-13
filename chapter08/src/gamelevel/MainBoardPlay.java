8568827085163372a2d9c191b3aac3a1adbf6ee0
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-14 08:28:22 +0900

    37. 인터페이스란

--- chapter08/src/gamelevel/MainBoardPlay.java
+++ chapter08/src/gamelevel/MainBoardPlay.java
@@ -1,8 +1,11 @@
 package gamelevel;
 
+import java.util.ArrayList;
+
 public class MainBoardPlay {
 
     public static void main(String[] args) {
+/*
         Player player = new Player();
         player.play(1);
 
@@ -13,5 +16,25 @@ public class MainBoardPlay {
         SuperLevel sLevel = new SuperLevel();
         player.upgradeLevel(sLevel);
         player.play(3);
+*/
+
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        arrayList1.add(1);
+        arrayList1.add(2);
+        arrayList1.add(3);
+
+//        ArrayList<Integer> arrayList2 = arrayList1;
+        ArrayList<Integer> arrayList2 = new ArrayList<>(arrayList1);
+        arrayList2.set(2, 5);
+
+        for (Integer i : arrayList1) {
+            System.out.println(i);
+        }
+
+        System.out.println("===========================");
+
+        for (Integer i : arrayList2) {
+            System.out.println(i);
+        }
     }
 }
--- chapter09/src/interfaceex/CalcTest.java
+++ chapter09/src/interfaceex/CalcTest.java
@@ -4,6 +4,7 @@ public class CalcTest {
 
     public static void main(String[] args) {
 
+        //다형성
         Calc calc = new CompleteCalc();
 //        Calculator calc = new CompleteCalc();
 //        CompleteCalc calc = new CompleteCalc();
@@ -14,12 +15,14 @@ public class CalcTest {
         System.out.println(calc.substract(n1, n2));
         System.out.println(calc.times(n1, n2));
         System.out.println(calc.divide(n1, n2));
+
+        //Calc calc = new CompleteCalc(); 에서는 사용 불가
 //        calc.showInfo();
 
-        calc.description();
+//        calc.description();
 
-        int[] arr = {1,2,3,4,5};
-        int sum = Calc.total(arr);
-        System.out.println(sum);
+//        int[] arr = {1,2,3,4,5};
+//        int sum = Calc.total(arr);
+//        System.out.println(sum);
     }
 }
